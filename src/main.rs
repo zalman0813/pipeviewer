1c98ad26a0c5ed0360cb123a2eebb99529a561b5
Eason <zalman0813@gmail.com>
2021-09-13 20:50:47 +0800

    removed unused data

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use clap::{App, Arg};
-use std::fs::File;
 use std::env;
-use std::io::{self, BufReader, BufWriter, ErrorKind, Result, Read, Write};
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter, ErrorKind, Read, Result, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let matches = App::new("pipeviewer")
         .arg(Arg::with_name("infile").help("Read from a file instead of stdin"))
-        .arg(Arg::with_name("outfile")
-                    .short("o")
-                    .long("outfile")
-                    .takes_value(true)
-                    .help("Write output to a file instead of stdout"))
-        .arg(Arg::with_name("silent")
-                        .short("s")
-                        .long("silent"))
+        .arg(
+            Arg::with_name("outfile")
+                .short("o")
+                .long("outfile")
+                .takes_value(true)
+                .help("Write output to a file instead of stdout"),
+        )
+        .arg(Arg::with_name("silent").short("s").long("silent"))
         .get_matches();
     let infile = matches.value_of("infile").unwrap_or_default();
     let outfile = matches.value_of("outfile").unwrap_or_default();
